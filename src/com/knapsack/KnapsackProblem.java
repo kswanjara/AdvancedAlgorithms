dd5c8d27ae07a8e38063cae66bfaff30f1741754
Kunal Wanjara <nuksw5@us.royalahold.net>
2019-04-10 16:33:22 -0400

    Changed the input to accept user input

---
+++ src/com/knapsack/KnapsackProblem.java
@@ -0,0 +1,62 @@
+package com.knapsack;
+
+import java.util.Scanner;
+
+/**
+ * This class holds the implementation of Unbounded Knapsack Problem.
+ *
+ * @author Kunal Wanjara
+ */
+public class KnapsackProblem {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Total number of weights : ");
+        int num = scanner.nextInt();
+
+        int[] wt = new int[num];
+        int[] val = new int[num];
+
+        System.out.println("Enter total " + num + " available weights(Press enter after each entry) : ");
+        for (int i = 0; i < num; i++) {
+            wt[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter total " + num + " values corresponding to each weight(Press enter after each entry) : ");
+        for (int i = 0; i < num; i++) {
+            val[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter capacity of Knapsack : ");
+
+        int W = scanner.nextInt();
+
+        System.out.println();
+//        int[] wt = {10, 20, 50};
+//        int[] val = {5, 5, 2};
+//        int W = 75;
+
+        System.out.println("Unbounded Knapsack Solution:");
+        System.out.println(unboundedKnapsack(wt, val, W) + "\n");
+
+        System.out.println("Fractional Knapsack Solution:");
+        System.out.println(unboundedKnapsack(wt, val, W) + "\n");
+    }
+
+
+    private static int unboundedKnapsack(int[] wt, int[] val, int W) {
+
+        int sub[] = new int[W + 1];
+
+        for (int i = 0; i < W + 1; i++) {
+            for (int j = 0; j < wt.length; j++) {
+                if (wt[j] <= i) {
+                    sub[i] = Math.max(sub[i], sub[i - wt[j]] +
+                            val[j]);
+                }
+            }
+        }
+        return sub[W];
+    }
+}
